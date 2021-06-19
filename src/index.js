1fdad8510b1deed6623afdfe4e2181bdfa6db6d1
parham <parhamhassanzade.p@gmail.com>
2021-06-20 03:51:07 +0430

    login panel created

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import {Login} from "./pages/Manager/Login/Login.component"
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+<Login/>,
 
+document.getElementById("root"));
 
 reportWebVitals();
---
+++ src/pages/Manager/Login/Login.component.js
@@ -0,0 +1,62 @@
+import { Component } from "react";
+class Login extends Component {
+  render() {
+    return (
+      <div
+        style={{ height: "100vh" }}
+        className={"d-flex justify-content-center align-items-center"}
+      >
+        <div
+          className="card d-flex justify-content-center align-items-center border-dark p-5"
+          style={{ width: "50rem" }}
+        >
+          <h2>ورود به پنل مدیریت فروشگاه پی شاپ</h2>
+          <form className="row  needs-validation " noValidate>
+            <div className="mb-3 row ">
+              <label className="form-label d-flex flex-row-reverse ">
+                {" "}
+                : نام کاربری
+              </label>
+              <input
+                type="text"
+                className="form-control border-dark"
+                id="exampleFormControlInput1"
+                required
+              />
+            </div>
+            <div className="mb-3 row ">
+              <label
+                htmlFor="exampleFormControlTextarea1"
+                className="form-label d-flex flex-row-reverse "
+              >
+                : رمز عبور
+              </label>
+              <input
+                className="form-control border-dark "
+                type="password"
+                name="password"
+                id="exampleFormControlTextarea1"
+                required
+              />
+            </div>
+
+            <div className=" d-flex justify-content-center align-items-center  ">
+              <button
+                type="button"
+                className="btn btn-primary"
+                data-bs-dismiss="modal"
+              >
+                ورود
+              </button>
+            </div>
+          </form>
+          <div className="d-flex justify-content-start" style={{width:"100%"}}>
+            <a style={{textDecoration:"none"}}href="#">بازگشت به سایت</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export { Login };
