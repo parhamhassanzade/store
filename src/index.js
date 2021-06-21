785aad89ecf52d255148a983e8b8a27310fb6e5b
parham <parhamhassanzade.p@gmail.com>
2021-06-21 22:35:54 +0430

    router bug on quantify panel fixed

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 
 import "bootstrap/dist/css/bootstrap.css";
-//import { Router } from "./route/App.router";
-import { Quntify } from "./pages/Manager/Quantity/Quantify.component";
+import { Router } from "./route/App.router";
+
 
 
 ReactDOM.render(
 
- //<Router />
- <Quntify/>
+ <Router />
+
 
 
 
