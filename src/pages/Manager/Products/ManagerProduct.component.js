26af2db82aea19d0c5a736593b9c532ef741634d
parham <parhamhassanzade.p@gmail.com>
2021-06-29 01:35:40 +0430

    show data in table and create pagination

--- src/pages/Manager/Products/ManagerProduct.component.js
+++ src/pages/Manager/Products/ManagerProduct.component.js
@@ -4,25 +4,43 @@ import { ManagerHeader } from "../../../layout/PageController";
 // import { axios } from "axios";
 class ManagerProduct extends Component {
   state = {
-    product: []
+    product: [],
   };
+
+  //? get all data
   async componentDidMount() {
-    const axios=require('axios')
-     await axios.get('http://localhost:3000/Products?parentgroup=لبنیات').then(respose=>{ this.setState({product:respose.data})
-   
-    }).catch(function (error) {
-      console.log(error);
-    })
+    const axios = require("axios");
+    await axios
+      .get("http://localhost:3000/Products")
+      .then((respose) => {
+        this.setState({ product: respose.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
+
+  CreateRow = () => {
+    return this.state.product.map((row, i) => {
+      return (
+        <tr key={i}>
+          <td>{row.id}</td>
+          <th scope="row">{row.name}</th>
+          <td>
+            {row.parentgroup}/{row.grop}
+          </td>
+          <td>
+            <a href="#">ویرایش</a>
+          </td>
+        </tr>
+      );
+    });
+  };
+
   render() {
     return (
       <>
-        
-        {
-          console.log("gvr",this.state.product)
-
-        }
- 
+        {}
 
         <ManagerHeader />
         <nav className="my-3 container d-flex justify-content-between">
@@ -39,27 +57,30 @@ class ManagerProduct extends Component {
                 <th scope="col">ویرایش</th>
               </tr>
             </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td>Larry the Bird</td>
-                <td>@twitter</td>
-                <td>@fat</td>
-              </tr>
-            </tbody>
+            <tbody>{this.CreateRow()}</tbody>
           </table>
+          <nav
+            className="d-flex justify-content-center mt-5"
+            aria-label="Page navigation example"
+          >
+            <ul class="pagination">
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  1
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  2
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  3
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </>
     );
