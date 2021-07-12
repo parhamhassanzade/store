09f05f7d229c1ccbc38168f4010eea7a7e257fb2
parham <parhamhassanzade.p@gmail.com>
2021-07-13 02:23:48 +0430

    Update Basket.component.js

--- src/pages/Customer/Basket/Basket.component.js
+++ src/pages/Customer/Basket/Basket.component.js
@@ -1,7 +1,57 @@
 import { Component } from "react";
+import { CustomerHeader } from "../../../layout/PageController";
 class Basket extends Component {
   render() {
-    return <div>basket page</div>;
+    return (
+      <wrapper>
+        <CustomerHeader />
+        <div className="container" dir="rtl">
+          <div class="row">
+            <h1>سبد خرید</h1>
+          </div>
+          <div className="row mt-5">
+            <table class="table col-sm table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">کالا</th>
+                  <th scope="col">قیمت</th>
+                  <th scope="col">تعداد</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Larry</td>
+                  <td>the Bird</td>
+                  <td>@twitter</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="row ">
+            <div class="col-sm">
+              <span>جمع کل: 125000 تومان</span>
+            </div>
+            <div class="col-sm d-flex justify-content-end">
+              <button className="btn btn-success">نهایی کردن سبد خرید</button>
+            </div>
+          </div>
+        </div>
+      </wrapper>
+    );
   }
 }
 
