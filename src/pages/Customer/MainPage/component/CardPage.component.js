9e9a2db1372cfdc5d320379a3146f2d3b56e63c9
parham <69312799+parhamhassanzade@users.noreply.github.com>
2021-09-27 22:23:17 +0330

    product card ui update

--- src/pages/Customer/MainPage/component/CardPage.component.js
+++ src/pages/Customer/MainPage/component/CardPage.component.js
@@ -1,5 +1,13 @@
 import { Component } from "react";
-import { Card, CardBody, CardTitle, CardText } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+} from "reactstrap";
 class CardPage extends Component {
   render() {
     return (
@@ -7,40 +15,33 @@ class CardPage extends Component {
         <Card
           style={{
             width: "250px",
+            height: "350px",
             boxShadow: "-1px 0px 20px 4px black ",
             borderRadius: "15px",
             margin: "0 10px 2px 10px",
-
+            overflow:"hidden"
           }}
         >
+          <CardImg
+            top
+            width="100%"
+            height="100%"
+            src={this.props.image}
+            alt="Card image cap"
+          />
           <CardBody className="d-flex justify-content-end">
-            <div
+          <div
               id="infoBox"
               className="d-flex flex-column align-items-end justify-content-center "
             >
-              <CardTitle tag="h6">{this.props.lable}</CardTitle>
-              <CardText>
-                <small className="text-muted">قیمت: {this.props.price} تومان </small>
-              </CardText>
-            </div>
-            <div
-              style={{
-                width: "9vw",
-                height: "10vh",
-                overflow: "hidden",
-              }}
-              id="imageBox"
-              className="d-flex justify-content-end"
-            >
-              <img
-                sytyle={{
-                  height: "100%",
-                  width: "100%",
-               
-                }}
-                src={this.props.image}
-              />
-            </div>
+            <CardTitle tag="h6">{this.props.lable}</CardTitle>
+            <CardText>
+              <small className="text-muted">
+                قیمت: {this.props.price} تومان{" "}
+              </small>
+            </CardText>
+           
+          </div>
           </CardBody>
         </Card>
       </>
